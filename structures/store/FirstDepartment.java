b5458bb5e36a6c337506505d703f78e05cae1247
Andreea010 <70898955+Andreea010@users.noreply.github.com>
2021-11-23 19:23:06 +0200

    Delete FirstDepartment.java

--- structures/store/FirstDepartment.java
+++
@@ -1,8 +0,0 @@
-package demo.models.structures.store;
-
-public class FirstDepartment implements DescriptionAPI {
-
-    public void description(String name, int IDNumber, String staff) {
-        System.out.println("|  " + name + "  | " + "id." + IDNumber + " |       " + staff + "      |");
-    }
-}
