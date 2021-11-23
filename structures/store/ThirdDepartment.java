e20a4bd817d1d929d3b13d4729b29700d12b75b8
Andreea010 <70898955+Andreea010@users.noreply.github.com>
2021-11-23 19:23:12 +0200

    Delete ThirdDepartment.java

--- structures/store/ThirdDepartment.java
+++
@@ -1,8 +0,0 @@
-package demo.models.structures.store;
-
-public class ThirdDepartment implements DescriptionAPI {
-
-    public void description(String name, int IDNumber, String staff) {
-        System.out.println("|  " + name + "  | " + "id." + IDNumber + " |     " + staff + "     |");
-    }
-}
